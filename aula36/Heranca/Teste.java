e674b7a14b363dcdbdaf153a1a6c0c4f98feeb4a
valerio-figueira <95040980+valerio-figueira@users.noreply.github.com>
2022-04-27 08:19:48 -0300

    atualizando curso

--- aula36/Heranca/Teste.java
+++ aula36/Heranca/Teste.java
@@ -14,23 +14,24 @@ public class Teste {
 		ArrayList<Double> media = new ArrayList<Double>();
 		double soma = 0;
 
-		// controller.definirCurso(c);
-		// controller.definirProfessor(p);
+		controller.definirCurso(c);
+		controller.definirProfessor(p);
 		controller.definirAlunos(c, new ArrayList<Aluno>());
-		// c.setProfessor(p);
-		// controller.definirMedia(c, media);
+		c.setProfessor(p);
 
-		for (int i = 0; i < c.getAlunos().size(); i++) {
-			showInfo("\nNome do aluno: " + c.getAlunos().get(i).getNome());
-			showInfo("\nNotas: ");
-			soma = 0;
-			for (int j = 0; j < c.getAlunos().get(i).getNotas().size(); j++) {
-				soma += c.getAlunos().get(i).getNotas().get(j);
-				showInfo(c.getAlunos().get(i).getNotas().get(j) + " - ");
-				if (j == c.getAlunos().get(i).getNotas().size() - 1) {
-					media.add(soma / c.getAlunos().get(i).getNotas().size());
-					showInfo("Média: " + media.get(i));
-					controller.definirAprovacao(c, media.get(i));
+		if (c.getAlunos() != null) {
+			for (int i = 0; i < c.getAlunos().size(); i++) {
+				showInfo("\nNome do aluno: " + c.getAlunos().get(i).getNome());
+				showInfo("\nNotas: ");
+				soma = 0;
+				for (int j = 0; j < c.getAlunos().get(i).getNotas().size(); j++) {
+					soma += c.getAlunos().get(i).getNotas().get(j);
+					showInfo(c.getAlunos().get(i).getNotas().get(j) + " - ");
+					if (j == c.getAlunos().get(i).getNotas().size() - 1) {
+						media.add(soma / c.getAlunos().get(i).getNotas().size());
+						showInfo("Média: " + media.get(i));
+						controller.definirAprovacao(c, media.get(i));
+					}
 				}
 			}
 		}
