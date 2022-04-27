bce971198d5da60622c5d9052b279ed92aa317d0
valerio-figueira <95040980+valerio-figueira@users.noreply.github.com>
2022-04-27 15:57:39 -0300

    atualizando

--- aula36/Heranca/Teste.java
+++ aula36/Heranca/Teste.java
@@ -1,5 +1,6 @@
 package com.valerio.cursojava.aula36.Heranca;
 
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 
 public class Teste {
@@ -12,12 +13,19 @@ public class Teste {
 		Controller controller = new Controller();
 		Professor p = new Professor();
 		ArrayList<Double> media = new ArrayList<Double>();
+		DecimalFormat df = new DecimalFormat("##.00");
 		double soma = 0;
 
 		controller.definirCurso(c);
 		controller.definirProfessor(p);
 		controller.definirAlunos(c, new ArrayList<Aluno>());
 		c.setProfessor(p);
+
+		showInfo("\nNome do Curso: " + c.getNome());
+		showInfo("\nHorário: " + c.getHorario());
+		showInfo("\nProfessor: " + c.getProfessor().getNome());
+		showInfo("\nDepartamento: " + c.getProfessor().getDepartamento());
+		showInfo("\nE-mail: " + c.getProfessor().getEmail());
 
 		if (c.getAlunos() != null) {
 			for (int i = 0; i < c.getAlunos().size(); i++) {
@@ -29,7 +37,7 @@ public class Teste {
 					showInfo(c.getAlunos().get(i).getNotas().get(j) + " - ");
 					if (j == c.getAlunos().get(i).getNotas().size() - 1) {
 						media.add(soma / c.getAlunos().get(i).getNotas().size());
-						showInfo("Média: " + media.get(i));
+						showInfo("Média: " + df.format(media.get(i)));
 						controller.definirAprovacao(c, media.get(i));
 					}
 				}
